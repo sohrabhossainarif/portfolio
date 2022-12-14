d11c917f68dae9b3eb934068dc6e844ecba15d22
Sohrab Hossain <you@example.com>
2022-12-14 21:25:42 +0600

    portfolio

---
+++ js/script.js
@@ -0,0 +1,51 @@
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = () =>{
+   menu.classList.toggle('fa-times');
+   header.classList.toggle('active');
+   document.body.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+   if(window.innerWidth < 991){
+      menu.classList.remove('fa-times');
+      header.classList.remove('active');
+      document.body.classList.remove('active');
+   }
+
+   document.querySelectorAll('section').forEach(sec =>{
+
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 150;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
+
+      if(top >= offset && top < offset + height){
+         document.querySelectorAll('.header .navbar a').forEach(links =>{
+            links.classList.remove('active');
+            document.querySelector('.header .navbar a[href*='+ id +']').classList.add('active')
+         });
+      };
+
+   });
+   
+}
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw-HCTyPER185osu3qW-JZ4vnG7BCraPnKpiFlLEKBEtdoswaaTQHjTfJFfjAzUQfkSGA/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+      msg.innerHTML = "Message sent successfilly"
+      setTimeout(function(){
+         msg.innerHTML =""
+      },2000)
+      form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
+
